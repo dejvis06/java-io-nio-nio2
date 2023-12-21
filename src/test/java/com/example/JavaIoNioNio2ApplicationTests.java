e03be3c449fa918ed77a33af5fd140a3dc45a36a
beqirajd <dejvis.beqiraj@linksmt.it>
2023-12-21 12:15:34 +0100

    clear code

--- src/test/java/com/example/JavaIoNioNio2ApplicationTests.java
+++ src/test/java/com/example/JavaIoNioNio2ApplicationTests.java
@@ -1,24 +1,15 @@
 package com.example;
 
-import com.example.io.ReadFile;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 
 @SpringBootTest
 class JavaIoNioNio2ApplicationTests {
 
-    @Value("classpath:roman_empire_history.txt")
-    Resource resource;
-
     @Test
     void contextLoads() throws IOException {
-
-        new ReadFile(1024, resource.getFile().getPath())
-                .perform(bytes -> System.err.println("reading bytes: " + new String(bytes)));
     }
 
 }

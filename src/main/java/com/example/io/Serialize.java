cd92bad48a230c4a5146e40c928e9c4a06f143ca
beqirajd <dejvis.beqiraj@linksmt.it>
2023-12-21 14:02:52 +0100

    add class level comment

--- src/main/java/com/example/io/Serialize.java
+++ src/main/java/com/example/io/Serialize.java
@@ -5,6 +5,24 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 
+/**
+ * The Serialize class provides utility methods for object serialization and deserialization.
+ * It includes methods to convert objects to byte arrays (serialization) and to reconstruct
+ * objects from byte arrays (deserialization).
+ * <p>
+ * This class uses Java's built-in serialization mechanism, allowing objects to be converted
+ * into a portable byte format, which can be transmitted over networks, stored, or reconstructed
+ * at a later time.
+ * <p>
+ * Usage Example:
+ * ```java
+ * // Serialization
+ * byte[] serializedData = Serialize.serialize(myObject);
+ * <p>
+ * // Deserialization
+ * MyObjectType deserializedObject = Serialize.deserialize(serializedData);
+ * ```
+ */
 public class Serialize {
 
     private static final Logger logger = LoggerFactory.getLogger(Serialize.class);

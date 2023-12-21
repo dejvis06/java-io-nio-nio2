810ccbcfb811cc79ce66c4dfcc568874e39b40b7
beqirajd <dejvis.beqiraj@linksmt.it>
2023-12-21 12:17:24 +0100

    remove exc from signature

--- src/test/java/com/example/JavaIoNioNio2ApplicationTests.java
+++ src/test/java/com/example/JavaIoNioNio2ApplicationTests.java
@@ -3,13 +3,11 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
-
 @SpringBootTest
 class JavaIoNioNio2ApplicationTests {
 
     @Test
-    void contextLoads() throws IOException {
+    void contextLoads() {
     }
 
 }

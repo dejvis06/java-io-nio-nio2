576d8cedad5d8530cd41e15a8d7825c14ef176d8
beqirajd <dejvis.beqiraj@linksmt.it>
2023-12-21 12:15:47 +0100

    add tests to separate class

---
+++ src/test/java/com/example/ReadFileTests.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import com.example.io.ReadFile;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+@SpringBootTest
+class ReadFileTests {
+
+    @Value("classpath:roman_empire_history.txt")
+    Resource resource;
+
+    @Test
+    void contextLoads() throws IOException {
+
+        new ReadFile(1024, resource.getFile().getPath())
+                .perform(bytes -> System.err.println("reading bytes: " + new String(bytes)));
+    }
+
+}
